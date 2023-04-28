6453f6f67ae6ef7d759a4bb445c88481bb3f8294
Elena Yaroma <yaroma.elena@gmail.com>
2023-04-28 13:20:58 +0400

    feat: add objects for types of keyboards and check language

---
+++ scripts/index.js
@@ -0,0 +1,316 @@
+// const of different type of keyboards
+const keysEn = {
+  Backquote: '`',
+  Digit1: '1',
+  Digit2: '2',
+  Digit3: '3',
+  Digit4: '4',
+  Digit5: '5',
+  Digit6: '6',
+  Digit7: '7',
+  Digit8: '8',
+  Digit9: '9',
+  Digit0: '0',
+  Minus: '-',
+  Equal: '=',
+  Backspace: 'Backspace',
+  // перенос
+  Tab: 'Tab',
+  KeyQ: 'q',
+  KeyW: 'w',
+  KeyE: 'e',
+  KeyR: 'r',
+  KeyT: 't',
+  KeyY: 'y',
+  KeyU: 'u',
+  KeyI: 'i',
+  KeyO: 'o',
+  KeyP: 'p',
+  BracketLeft: '[',
+  BracketRight: ']',
+  Backslash: '\\',
+  // перенос
+  CapsLock: 'CapsLock',
+  KeyA: 'a',
+  KeyS: 's',
+  KeyD: 'd',
+  KeyF: 'f',
+  KeyG: 'g',
+  KeyH: 'h',
+  KeyJ: 'j',
+  KeyK: 'k',
+  KeyL: 'l',
+  Semicolon: ';',
+  Quote: "'",
+  Enter: 'Enter',
+  // перенос
+  ShiftLeft: 'Shift',
+  KeyZ: 'z',
+  KeyX: 'x',
+  KeyC: 'c',
+  KeyV: 'v',
+  KeyB: 'b',
+  KeyN: 'n',
+  KeyM: 'm',
+  Comma: ',',
+  Period: '.',
+  Slash: '/',
+  ShiftRight: 'Shift',
+  // перенос
+  ControlLeft: 'Control',
+  MetaLeft: 'Meta',
+  AltLeft: 'Alt',
+  Space: ' ',
+  AltRight: 'Alt',
+  ControlRight: 'Control',
+  ArrowLeft: 'ArrowLeft',
+  ArrowUp: 'ArrowUp',
+  ArrowDown: 'ArrowDown',
+  ArrowRight: 'ArrowRight',
+};
+const keysEnShift = {
+  Backquote: '~',
+  Digit1: '!',
+  Digit2: '@',
+  Digit3: '#',
+  Digit4: '$',
+  Digit5: '%',
+  Digit6: '^',
+  Digit7: '&',
+  Digit8: '*',
+  Digit9: '(',
+  Digit0: ')',
+  Minus: '_',
+  Equal: '+',
+  Backspace: 'Backspace',
+  // перенос
+  Tab: 'Tab',
+  KeyQ: 'Q',
+  KeyW: 'W',
+  KeyE: 'E',
+  KeyR: 'R',
+  KeyT: 'T',
+  KeyY: 'Y',
+  KeyU: 'U',
+  KeyI: 'I',
+  KeyO: 'O',
+  KeyP: 'P',
+  BracketLeft: '{',
+  BracketRight: '}',
+  Backslash: '|',
+  // перенос
+  CapsLock: 'CapsLock',
+  KeyA: 'A',
+  KeyS: 'S',
+  KeyD: 'D',
+  KeyF: 'F',
+  KeyG: 'G',
+  KeyH: 'H',
+  KeyJ: 'J',
+  KeyK: 'K',
+  KeyL: 'L',
+  Semicolon: ':',
+  Quote: '"',
+  Enter: 'Enter',
+  // перенос
+  ShiftLeft: 'Shift',
+  KeyZ: 'Z',
+  KeyX: 'X',
+  KeyC: 'C',
+  KeyV: 'V',
+  KeyB: 'B',
+  KeyN: 'N',
+  KeyM: 'M',
+  Comma: '<',
+  Period: '>',
+  Slash: '?',
+  ShiftRight: 'Shift',
+  // перенос
+  ControlLeft: 'Control',
+  MetaLeft: 'Meta',
+  AltLeft: 'Alt',
+  Space: ' ',
+  AltRight: 'Alt',
+  ControlRight: 'Control',
+  ArrowLeft: 'ArrowLeft',
+  ArrowUp: 'ArrowUp',
+  ArrowDown: 'ArrowDown',
+  ArrowRight: 'ArrowRight',
+};
+const keysRu = {
+  Backquote: 'ё',
+  Digit1: '1',
+  Digit2: '2',
+  Digit3: '3',
+  Digit4: '4',
+  Digit5: '5',
+  Digit6: '6',
+  Digit7: '7',
+  Digit8: '8',
+  Digit9: '9',
+  Digit0: '0',
+  Minus: '-',
+  Equal: '=',
+  Backspace: 'Backspace',
+  // перенос
+  Tab: 'Tab',
+  KeyQ: 'й',
+  KeyW: 'ц',
+  KeyE: 'у',
+  KeyR: 'к',
+  KeyT: 'е',
+  KeyY: 'н',
+  KeyU: 'г',
+  KeyI: 'ш',
+  KeyO: 'щ',
+  KeyP: 'з',
+  BracketLeft: 'х',
+  BracketRight: 'ъ',
+  Backslash: '\\',
+  // перенос
+  CapsLock: 'CapsLock',
+  KeyA: 'ф',
+  KeyS: 'ы',
+  KeyD: 'в',
+  KeyF: 'а',
+  KeyG: 'п',
+  KeyH: 'р',
+  KeyJ: 'о',
+  KeyK: 'л',
+  KeyL: 'д',
+  Semicolon: 'ж',
+  Quote: 'э',
+  Enter: 'Enter',
+  // перенос
+  ShiftLeft: 'Shift',
+  KeyZ: 'я',
+  KeyX: 'ч',
+  KeyC: 'с',
+  KeyV: 'м',
+  KeyB: 'и',
+  KeyN: 'т',
+  KeyM: 'ь',
+  Comma: 'б',
+  Period: 'ю',
+  Slash: '.',
+  ShiftRight: 'Shift',
+  // перенос
+  ControlLeft: 'Control',
+  MetaLeft: 'Meta',
+  AltLeft: 'Alt',
+  Space: ' ',
+  AltRight: 'Alt',
+  ControlRight: 'Control',
+  ArrowLeft: 'ArrowLeft',
+  ArrowUp: 'ArrowUp',
+  ArrowDown: 'ArrowDown',
+  ArrowRight: 'ArrowRight',
+};
+const keysRuShift = {
+  Backquote: 'Ё',
+  Digit1: '!',
+  Digit2: '"',
+  Digit3: '№',
+  Digit4: ';',
+  Digit5: '%',
+  Digit6: ':',
+  Digit7: '?',
+  Digit8: '*',
+  Digit9: '(',
+  Digit0: ')',
+  Minus: '_',
+  Equal: '+',
+  Backspace: 'Backspace',
+  // перенос
+  Tab: 'Tab',
+  KeyQ: 'Й',
+  KeyW: 'Ц',
+  KeyE: 'У',
+  KeyR: 'К',
+  KeyT: 'Е',
+  KeyY: 'Н',
+  KeyU: 'Г',
+  KeyI: 'Ш',
+  KeyO: 'Щ',
+  KeyP: 'З',
+  BracketLeft: 'Х',
+  BracketRight: 'Ъ',
+  Backslash: '/',
+  // перенос
+  CapsLock: 'CapsLock',
+  KeyA: 'Ф',
+  KeyS: 'Ы',
+  KeyD: 'В',
+  KeyF: 'А',
+  KeyG: 'П',
+  KeyH: 'Р',
+  KeyJ: 'О',
+  KeyK: 'Л',
+  KeyL: 'Д',
+  Semicolon: 'Ж',
+  Quote: 'Э',
+  Enter: 'Enter',
+  // перенос
+  ShiftLeft: 'Shift',
+  KeyZ: 'Я',
+  KeyX: 'Ч',
+  KeyC: 'С',
+  KeyV: 'М',
+  KeyB: 'И',
+  KeyN: 'Т',
+  KeyM: 'Ь',
+  Comma: 'Б',
+  Period: 'Ю',
+  Slash: ',',
+  ShiftRight: 'Shift',
+  // перенос
+  ControlLeft: 'Control',
+  MetaLeft: 'Meta',
+  AltLeft: 'Alt',
+  Space: ' ',
+  AltRight: 'Alt',
+  ControlRight: 'Control',
+  ArrowLeft: 'ArrowLeft',
+  ArrowUp: 'ArrowUp',
+  ArrowDown: 'ArrowDown',
+  ArrowRight: 'ArrowRight',
+};
+
+// check saved language and set default language
+let lang = localStorage.getItem('lang');
+if (!lang) {
+  localStorage.setItem('lang', 'en');
+  lang = 'en';
+}
+
+// create and append titles
+const title = document.createElement('h1');
+title.textContent = 'Виртуальная клавиатура';
+// title.textContent = lang === 'en' ? 'Virtual keyboard' : 'Виртуальная клавиатура';
+const subtitle = document.createElement('p');
+// subtitle.textContent = lang === 'en'
+//   ? 'The keyboard was created in the Windows operating system'
+//   : 'Клавиатура создана в операционной системе Windows';
+subtitle.textContent = 'Клавиатура создана в операционной системе Windows';
+const info = document.createElement('p');
+info.textContent = 'Для переключения языка комбинация: левые Shift + Alt';
+
+document.body.appendChild(title);
+document.body.appendChild(subtitle);
+document.body.appendChild(info);
+
+let keyboard = lang === 'en' ? keysEn : keysRu;
+
+const keydiv = document.createElement('div');
+
+for (const [key, value] of Object.entries(keyboard)) {
+  const item = document.createElement('button');
+  item.textContent = value;
+  keydiv.appendChild(item);
+  if (key === 'Backspace' || key === 'Backslash' || key === 'Enter' || key === 'ShiftRight') {
+    const newLine = document.createElement('br');
+    keydiv.appendChild(newLine);
+  }
+}
+
+document.body.appendChild(keydiv);
